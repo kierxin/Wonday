6d46af1928638d2c8f5efbd56016da2ff0814392
Kira Porter <kierxin@gmail.com>
2021-09-05 17:15:19 -0400

    Write more session-related actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -3,23 +3,32 @@ import { postUser, postSession, deleteSession } from "../util/session_api_util";
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 
-const receiveCurrentUser = () => ({
+const receiveCurrentUser = currentUser => ({
     type: RECEIVE_CURRENT_USER,
-    user: user
+    currentUser: currentUser
 });
 
 const logoutCurrentUser = () => ({
     type: LOGOUT_CURRENT_USER
 });
 
+const receiveError = errors => ({
+    type: RECEIVE_SESSION_ERRORS
+});
+
 
 export const createNewUser = formUser => dispatch => postUser(formUser)
-    .then(user => dispatch(receiveCurrentUser(user)));
+    .then(user => dispatch(receiveCurrentUser(user)),
+        errors => dispatch(receiveError(errors.responseJSON))
+    );
 
 export const login = formUser => dispatch => postSession(formUser)
-    .then(user => dispatch(receiveCurrentUser(user)));
+    .then(user => dispatch(receiveCurrentUser(user)),
+        errors => dispatch(receiveError(errors.responseJSON))
+    );
 
 export const logout = () => dispatch => deleteSession()
     .then(() => dispatch(logoutCurrentUser()));
