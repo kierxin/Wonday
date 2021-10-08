71b394e565dc2bdad613393c342e6d243b4f951c
Kira Porter <kierxin@gmail.com>
2021-10-08 19:04:12 -0400

    Add .cur extension in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
                 },
             },
             {
-                test: /\.(png|jpg|gif)$/i,
+                test: /\.(png|jpg|gif|cur)$/i,
                 use: [
                     {
                         loader: 'url-loader',
